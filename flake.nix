12023e34391c4461883518e34598427264b5f860
Xuân Trường <alonesera1612@gmail.com>
2026-01-09 17:02:02 +0700

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "NixOS Hyprland + Home Manager (no theme)";
+  description = "NixOS Hyprland + Home Manager (Acer Nitro 5 AN515-55)";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
